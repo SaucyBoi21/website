c115c3a38f53315b14990e6e0c5e267971e36fb1
Saahas Swaroop <saahas.swaroop@googlemail.com>
2024-12-15 16:08:08 -0500

    created fileskeletons and components

---
+++ app/src/app/page.tsx
@@ -0,0 +1,20 @@
+import Icon from "./components/icon";
+import Navbar from "./components/navbar";
+import Project from "./components/project";
+
+export default function Home() {
+  return (
+    <div>
+      <Project>
+
+      </Project>
+      <Navbar>
+
+      </Navbar>
+
+      <Icon>
+
+      </Icon>
+    </div>
+  )
+}
