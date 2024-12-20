c0ec40d657912f1d04a3cf23cbf6ec5ad3d6ee3f
Saahas Swaroop <saahas.swaroop@googlemail.com>
2024-12-20 13:18:18 -0500

    navbar

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,17 +4,11 @@ import Project from "./components/project";
 
 export default function Home() {
   return (
-    <div>
-      <Project>
-
-      </Project>
-      <Navbar>
-
-      </Navbar>
-
-      <Icon>
-
-      </Icon>
-    </div>
+    <>
+      <Navbar />
+      <Icon />
+      <Project /> 
+    
+    </>
   )
 }
